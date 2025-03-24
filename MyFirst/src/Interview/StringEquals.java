b9462e495c5b6527b345dd144e4e29e587114c53
Samrat <samrat.cs14@gmail.com>
2025-03-24 20:05:33 +0530

    Initial commit

--- MyFirst/src/Interview/StringEquals.java
+++ MyFirst/src/Interview/StringEquals.java
@@ -5,8 +5,10 @@ public class StringEquals {
         String a = "sam"; // can't be changed immutable
         String b = "sam";
         String c = "Sam";
+        String d = new String("sam");
 
         System.out.println(a == b);
+        System.out.println(a == d);
         System.out.println("a.equals(b) >" + a.equals(b));
         System.out.println(a == c);
         System.out.println("a.equals(c) >" + a.equals(c));
